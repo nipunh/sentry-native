3ac408a60f31917845348c7435566cbff7ffc8b0
nipunh <nipunhedaoo@gmail.com>
2024-12-22 16:03:37 -0500

    Update example.c

--- examples/example.c
+++ examples/example.c
@@ -5,8 +5,9 @@ int main(void) {
         sentry_options_t *options = sentry_options_new();
     sentry_options_set_dsn(options, "https://15160f69b5dc9faa778f1d8ee9358b0e@o4508507428749312.ingest.us.sentry.io/4508508876374016");
     sentry_options_set_debug(options, 1); // Enable debug output
+    sentry_options_set_handler_path(options, "/Users/nipunhedaoo/Documents/GitHub/sentry-native/build/crashpad_build/handler/crashpad_handler");
+    sentry_init(options);
 
-    sentry_options_set_handler_path(options, "../crashpad_build/handler/crashpad_handler");
 
     if (sentry_init(options) != 0) {
         fprintf(stderr, "Sentry initialization failed!\n");
